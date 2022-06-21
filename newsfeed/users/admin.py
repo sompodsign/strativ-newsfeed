60dab6c64450d6d32d767131c6f97886db4c9e98
Shampad Sharkar <sompodsign@gmail.com>
2022-06-22 03:28:50 +0600

    extend abstract user class fields

--- newsfeed/users/admin.py
+++ newsfeed/users/admin.py
@@ -15,7 +15,7 @@ class UserAdmin(auth_admin.UserAdmin):
     add_form = UserAdminCreationForm
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("name", "email")}),
+        (_("Personal info"), {"fields": ("name", "email", "first_name", "last_name")}),
         (
             _("Permissions"),
             {
@@ -30,5 +30,5 @@ class UserAdmin(auth_admin.UserAdmin):
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
-    list_display = ["username", "name", "is_superuser"]
+    list_display = ["username", "email", "first_name", "last_name", "name", "is_superuser"]
     search_fields = ["name"]
--- newsfeed/users/forms.py
+++ newsfeed/users/forms.py
@@ -1,5 +1,6 @@
 from allauth.account.forms import SignupForm
 from allauth.socialaccount.forms import SignupForm as SocialSignupForm
+from django import forms
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
@@ -32,6 +33,18 @@ class UserSignupForm(SignupForm):
     Default fields will be added automatically.
     Check UserSocialSignupForm for accounts created from social.
     """
+    first_name = forms.CharField(max_length=30, label='First Name',
+                                 widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=30, label='Last Name',
+                                widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+
+    def save(self, request):
+        user = super().save(request)
+
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+        return user
 
 
 class UserSocialSignupForm(SocialSignupForm):
