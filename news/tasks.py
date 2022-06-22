cbf0bbb6d906d36470d8d116764564277ff9b1b9
Shampad Sharkar <sompodsign@gmail.com>
2022-06-23 01:50:20 +0600

    define task -crate_news- to run periodically

--- news/tasks.py
+++ news/tasks.py
@@ -1,5 +1,33 @@
 from django.contrib.auth import get_user_model
-from django.core.mail import send_mail
+from news.utils import get_top_headlines
 
 from config import celery_app
 
+from news.models import NewsSetting, Article
+
+
+@celery_app.task
+def curate_news():
+    """
+    Task which curates news periodically. - 15minutes
+    """
+    users = get_user_model().objects.filter(is_active=True)
+    for user in users:
+        news_setting = NewsSetting.objects.get(user=user)
+        articles = list(Article.objects.filter(owner=user).values_list('title', flat=True))
+
+        if news_setting.is_active:
+            country = news_setting.country
+            sources = news_setting.sources
+            top_headlines = get_top_headlines(country, sources)
+
+            for top_headline in top_headlines:
+                if top_headline['title'] not in articles:
+                    Article.objects.create(
+                        owner=user,
+                        title=top_headline['title'],
+                        thumbnail=top_headline['urlToImage'],
+                        url=top_headline['url'],
+                        sources=top_headline['source']['name'],
+                        country=top_headline['source']['name']
+                    )
